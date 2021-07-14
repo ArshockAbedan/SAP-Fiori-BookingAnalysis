5b97be11cfedce29babdabed4d84ade6ecba8c39
Aref Abedjooy <arefabedjooy@gmail.com>
2021-07-13 21:47:03 -0400

    first commit

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(['sap/suite/ui/generic/template/lib/AppComponent'], function(AppComponent) {
+    return AppComponent.extend("sap.fe.demo.bookinganalysis.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
